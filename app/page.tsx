05f1c67ae95c31352d463ba23cee6d8094bbee8a
harshit <harshit.pant01@loblaw.ca>
2024-04-22 09:45:07 -0400

    fix formatting in Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,10 @@ export default function Home() {
   return (
     <div className="bg-white h-screen flex min-h-screen flex-col">
       <Header />
-
       <div className="flex font-sans pt-2 text-gray-700 items-center justify-center text-xl md:text-3xl font-bold text-gray-800">
         Draw your eSignature here
       </div>
       <SignatureCreator />
-
       <Footer />
     </div>
   );
